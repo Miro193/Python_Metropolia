8106d6cbc6b0e7ffb88e8fbd2523837ee2844ed0
Miro193 <mirostonezone@gmail.com>
2024-08-21 19:23:55 +0300

    Muutoksia tiedostoon

--- Mod1/moimaailma.py
+++ Mod1/moimaailma.py
@@ -1,1 +1,8 @@
+# Oli vanha PyCharm CE koneella, kun latasin pro version koneelle.
+# Jostain syystä ei löytänyt python-tiedostoja. Poistin ja latasin uusiksi PyCharmin,
+# niin rupesi homma toimimaan.
+# Oli valmiina pythonit ja git koneella tarkistin versiota, että varmasti löytyy.
+# Github profiili löyty ja vielä laitoin PyCharmin keskustelemaan githubin kanssa.
+
+print("Hei, maailma!")
 print("Moi, Miro!")
