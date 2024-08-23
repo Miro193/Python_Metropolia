12c4426fb92b211b7e76dfc8b4c10c6d6d29793c
Miro193 <mirostonezone@gmail.com>
2024-08-23 20:00:07 +0300

    korjattu tulostuksen kirjoitusvirhe

--- Mod2/Ympyran_saateen_pinta-ala_2_2.py
+++ Mod2/Ympyran_saateen_pinta-ala_2_2.py
@@ -4,4 +4,4 @@ import math
 
 sade = int(input("Anna ympyrän säde senttimetreinä: "))
 pinta_ala = math.pi*(sade**2)
-print(f"Ympyrän säteen pinta-ala on:\n{pinta_ala:.1f} cm^2.")
+print(f"Ympyrän pinta-ala on:\n{pinta_ala:.1f} cm^2.")
