40f60016ca77b124ff1c523dc9c1f18c15cc6daa
Miro193 <mirostonezone@gmail.com>
2024-10-24 20:51:00 +0300

    parannuksia

--- mod9/class_auto_9_3.py
+++ mod9/class_auto_9_3.py
@@ -3,3 +3,36 @@
 # tuntimäärässä edennyt. Esimerkki: auto-olion tämänhetkinen kuljettu matka on 2000 km. 
 # Nopeus on 60 km/h. Metodikutsu auto.kulje(1.5) kasvattaa kuljetun matkan lukemaan 2090 km.
 
+class Auto:
+
+	def __init__(self, rekisteritunnus, huippunopeus, hetkellinen_nopeus = 0, kuljettu_matka = 0):
+		self.rekisteritunnus = rekisteritunnus
+		self.huippunopeus = huippunopeus
+		self.hetkellinen_nopeus = hetkellinen_nopeus
+		self.kuljettu_matka = kuljettu_matka
+
+	def kiihdytaa(self, nopeuden_muutos):
+		if nopeuden_muutos > 0 and nopeuden_muutos <= self.huippunopeus and self.hetkellinen_nopeus <= self.huippunopeus:
+			self.hetkellinen_nopeus += nopeuden_muutos
+		else:
+			self.hetkellinen_nopeus = 0
+
+	
+	def kulje(self, tuntimaara):
+		self.kuljettu_matka += self.hetkellinen_nopeus * tuntimaara
+
+
+
+uusi_auto = Auto("ABC-123", 142, 0, 2000)
+
+print(f"""\nrekisterinumero = {uusi_auto.rekisteritunnus}
+huippunopeus = {uusi_auto.huippunopeus} km/h
+hetkellinennopeus = {uusi_auto.hetkellinen_nopeus} km/h
+kuljettu matka = {uusi_auto.kuljettu_matka} km""")
+print("\n---------------\n")
+
+uusi_auto.kiihdytaa(60)
+print(f"Auton nopeus = {uusi_auto.hetkellinen_nopeus} km/h")
+
+uusi_auto.kulje(1.5)
+print(f"Auton tämän hetken kuljettu matka on {uusi_auto.kuljettu_matka} km")
--- mod9/class_auto_9_4.py
+++ mod9/class_auto_9_4.py
@@ -10,4 +10,52 @@
 
 # Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä. 
 # Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
+from random import randint, randrange
 
+class Auto:
+
+	def __init__(self, rekisteritunnus, huippunopeus, hetkellinen_nopeus = 0, kuljettu_matka = 0):
+		self.rekisteritunnus = rekisteritunnus
+		self.huippunopeus = huippunopeus
+		self.hetkellinen_nopeus = hetkellinen_nopeus
+		self.kuljettu_matka = kuljettu_matka
+
+	def kiihdytaa(self, nopeuden_muutos):
+		if nopeuden_muutos > 0 and nopeuden_muutos <= self.huippunopeus and self.hetkellinen_nopeus <= self.huippunopeus:
+			self.hetkellinen_nopeus += nopeuden_muutos
+		
+		else:
+			self.hetkellinen_nopeus = 0
+
+	def kulje(self, tuntimaara):
+		self.kuljettu_matka += self.hetkellinen_nopeus * tuntimaara
+
+
+kilpa_autot = []
+matka = 0
+for i in range(1,11):
+	numero = i
+	nopeus = randint(100,200)
+	kilpa_auto = Auto(f"ABC-{numero}", nopeus)
+	kilpa_autot.append(kilpa_auto)
+	
+
+while matka <= 1000:
+
+	for h in kilpa_autot[:]:
+		h.kiihdytaa(randrange(-10,15))
+		h.kulje(1)
+		print(h.hetkellinen_nopeus, h.rekisteritunnus, h.kuljettu_matka)
+		if h.kuljettu_matka > matka:
+			matka = h.kuljettu_matka
+
+for k in kilpa_autot[:]:
+	print("------------------------------")
+	print(f"  rekisterinumero = {k.rekisteritunnus}\n  huippunopeus = {k.huippunopeus} km/h\n  hetkellinennopeus = {k.hetkellinen_nopeus} km/h\n  kuljettu matka = {k.kuljettu_matka} km")
+
+
+# uusi_auto.kiihdytaa(60)
+# print(f"Auton nopeus = {uusi_auto.hetkellinen_nopeus} km/h")
+
+# uusi_auto.kulje(1.5)
+# print(f"Auton tämän hetken kuljettu matka on {uusi_auto.kuljettu_matka} km")
